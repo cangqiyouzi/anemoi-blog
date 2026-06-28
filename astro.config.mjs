da31ae2b90a476194e6b8a928d4e49a791825aa3
cangqiyouzi <102418073+cangqiyouzi@users.noreply.github.com>
2026-06-28 19:09:25 +0800

    fix(config): 更新站点地址和基础路径配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   // 部署前替换为你的实际信息
-  site: 'https://your-username.github.io',
-  base: '/',
-  //base: '/anemoi-blog',
+  site: 'https://cangqiyouzi.github.io',
+  //base: '/',
+  base: '/anemoi-blog',
 });
